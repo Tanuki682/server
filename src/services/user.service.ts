50261db7918a00f9c66237b3f8256adba5a12a6b
Tanuki682 <thanawant2213@gmail.com>
2024-11-26 17:32:00 +0700

    init

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,8 +1,14 @@
-import { get } from "mongoose"
+import mongoose, { get, RootFilterQuery } from "mongoose"
 import { updateProfile, user, userPagination, userPaginator } from "../types/user.type"
+import { IUserDocument } from "../interfaces/user.interface"
+import { QueryHelper } from "../helper/query.helper"
 
 export const UserService = {
     get: function (pagination: userPagination, user_id: string): Promise<userPaginator> {
+        let filter: RootFilterQuery<IUserDocument> = {
+            _id: { $nin: new mongoose.Types.ObjectId(user_id) },
+            $and: QueryHelper.parseUserQuery(pagination)
+        }
         throw new Error('not implement')
     },
     getByUserName: function (username: string): Promise<user> {
