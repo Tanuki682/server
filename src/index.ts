7fbdc31f67d01a45ca66ca1e4244a8487016ca2e
Tanuki682 <thanawant2213@gmail.com>
2024-12-11 01:37:33 +0700

    correct error bun

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import { Elysia, t } from "elysia"
-import { example } from "./contrallers/examplt.contraller"
 import { swaggerConfig } from "./configs/swagger.config"
 import { tlsConfig } from "./configs/tls.config"
-
 import { MongoDB } from "./configs/database.config"
-import { ElysiaConfig } from "elysia"
+import cors from "@elysiajs/cors"
 import { jwtConfig } from "./configs/jwt.config"
 import { AccountContraller } from "./contrallers/account.contraller"
 import { UserContller } from "./contrallers/user.contloller"
@@ -16,12 +14,11 @@ const app = new Elysia()
   .use(staticPlugin())
 
   .use(swaggerConfig)
-  .use(example)
-  .use(core())
+  // .use(example)
+  .use(cors())
   .use(jwtConfig)
   .use(AccountContraller)
   .use(UserContller)
-
   .listen({
     port: Bun.env.PORT || 8000,
     tls: tlsConfig
@@ -31,7 +28,6 @@ let protocol = 'http'
 if ('cert' in tlsConfig)
   protocol = 'https'
 console.log(`🦊 Elysia is running at ${protocol}://${app.server?.hostname}:${app.server?.port}`)
-function core(): any {
-  throw new Error("Function not implemented.")
-}
+
+
 
